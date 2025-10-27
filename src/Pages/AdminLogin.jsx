1db5f70e42dbeaab2d280005dd4da03be12db8a6
ozancandirek <ozan_direk@hotmail.com>
2025-10-27 13:03:57 +0300

    AdminLogin import path fix

--- src/Pages/AdminLogin.jsx
+++ src/Pages/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../api/axios";
+import api from "../api/axiosConfig";
 
 function AdminLogin() {
   const [credentials, setCredentials] = useState({
